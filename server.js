f11e31dca27b1c21607c4b5e6c0516bd6a8e3b6c
Miguel Angel Rodríguez Padilla <ma003r52@gmail.com>
2024-11-24 14:37:33 -0600

    a

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const promocionesRoutes = require('./routes/promociones');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Middleware para procesar JSON
+// Middleware
 app.use(express.json());
 
 // Rutas
 app.use('/api/promociones', promocionesRoutes);
 
+// Iniciar servidor
+const PORT = process.env.PORT || 3000; // Vercel establece el puerto automáticamente
 app.listen(PORT, () => {
-    console.log(`Microservicio corriendo en http://localhost:${PORT}/api/promociones`);
+    console.log(`Microservicio corriendo en el puerto ${PORT}`);
 });
